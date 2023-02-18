23f668077a72772862ff750083bdcf50a3443569
@tsivaeva <tatiana_sivaeva@epam.com>
2023-02-18 12:35:47 +0400

    delete 1 import

--- src/test/java/com/epam/tc/hw3/utils/DriverManager.java
+++ src/test/java/com/epam/tc/hw3/utils/DriverManager.java
@@ -3,13 +3,17 @@ package com.epam.tc.hw3.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     public WebDriver setupDriver(String url) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--no-sandbox");
         setupChromeDriver();
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
+        WebDriver webDriver = new ChromeDriver(options);
+        webDriver.manage().window();
         webDriver.get(url);
         return webDriver;
     }
