a9be4e6711c0badd5be18a57c65589e58a2e92b6
@tsivaeva <tatiana_sivaeva@epam.com>
2023-02-18 17:31:19 +0400

    "--disable-dev-shm-usage"

--- src/test/java/com/epam/tc/hw3/utils/DriverManager.java
+++ src/test/java/com/epam/tc/hw3/utils/DriverManager.java
@@ -10,7 +10,7 @@ public class DriverManager {
     public WebDriver setupDriver(String url) {
         setupChromeDriver();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--no-sandbox", "--ignore-certificate-errors");
+        options.addArguments("--headless", "--no-sandbox", "--ignore-certificate-errors", "--disable-dev-shm-usage");
         WebDriver webDriver = new ChromeDriver(options);
         webDriver.manage().window().maximize();
         webDriver.get(url);
